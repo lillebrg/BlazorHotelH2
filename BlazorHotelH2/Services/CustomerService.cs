253b710dd85094351c795497958a4cf29a2f72ae
Mads Møller <102755527+TGSXVIII@users.noreply.github.com>
2023-09-19 13:13:56 +0200

    customerService again

--- BlazorHotelH2/Services/CustomerService.cs
+++ BlazorHotelH2/Services/CustomerService.cs
@@ -1,6 +1,38 @@
-﻿namespace BlazorHotelH2.Services
+﻿using static System.Net.WebRequestMethods;
+using BlazorHotelH2.Pages;
+using DomainModels;
+using Newtonsoft.Json;
+using Microsoft.Identity.Client;
+
+namespace BlazorHotelH2.Services
 {
     public class CustomerService
     {
+        string customerApi = https://localhost:7036/api/Customers
+
+        public async Task<bool> PostCustomerAsync()
+        {
+            HttpClient customerClient = new HttpClient();
+
+            HttpResponseMessage response = new HttpResponseMessage();
+
+            try
+            {
+                response = await customerClient.GetAsync(customerApi);
+                if (!response.IsSuccessStatusCode)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+
+            return false;
+        }
     }
 }
