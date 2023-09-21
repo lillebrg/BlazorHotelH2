6243805c1407b6140554853c7c9be02c47a1158c
Mads Møller <102755527+TGSXVIII@users.noreply.github.com>
2023-09-21 14:55:00 +0200

    updates

--- BlazorHotelH2/Program.cs
+++ BlazorHotelH2/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+//builder.Services.AddSingleton<AppContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
