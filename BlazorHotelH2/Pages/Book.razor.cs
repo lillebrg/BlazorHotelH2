5d5df5c16876192ac5f6b464e253ce8cc2a8cdd2
Mads Møller <102755527+TGSXVIII@users.noreply.github.com>
2023-09-29 09:04:09 +0200

    Booking front page, booking, backend, booking services

---
+++ BlazorHotelH2/Pages/Book.razor.cs
@@ -0,0 +1,25 @@
+﻿using BlazorHotelH2.Services;
+using DomainModels;
+
+namespace BlazorHotelH2.Pages
+{
+    public partial class BookRoom
+    {
+        public Booking booking = new Booking();
+        public void SubmitBooking()
+        {
+            BookingService bookingService = new BookingService();
+
+            try
+            {
+                bookingService.PostBookingAsync(booking);
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+            Console.WriteLine("done");
+        }
+    }
+}
