5375471176b6fb051cc2785d11fa4eade5535acf
BuildTools <unconfigured@null.spigotmc.org>
2023-09-29 09:02:16 +0200

    changes to login and messing around wth adminroomedit

--- BlazorHotelH2/Pages/Login.razor.cs
+++ BlazorHotelH2/Pages/Login.razor.cs
@@ -1,6 +1,24 @@
-﻿namespace BlazorHotelH2.Pages
+﻿using BlazorHotelH2.Services;
+using DomainModels;
+
+namespace BlazorHotelH2.Pages
 {
     public partial class Login
     {
+        public Customer customer = new Customer();
+
+        public void SubmitForm()
+        {
+            CustomerService customerService = new CustomerService();
+            try
+            {
+                customerService.PostCustomerAsync(customer);
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
     }
 }
